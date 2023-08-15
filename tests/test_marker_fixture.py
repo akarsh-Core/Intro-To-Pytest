9c4d00fe326112c11a4809078baa15378290be2d
Coredge.io <coredge.io@Coredgeios-MacBook-Pro.local>
2023-08-16 01:10:55 +0530

    basic pytest intro cases

---
+++ tests/test_marker_fixture.py
@@ -0,0 +1,69 @@
+"""
+This module purpose is to show how to use the markers and fixtures available in pytest framework by example.
+"""
+
+# --------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------
+
+import pytest
+from stuff.accum import Accumulator
+
+
+# --------------------------------------------------------------------------------
+# Fixtures
+# --------------------------------------------------------------------------------
+
+@pytest.fixture
+def accum():
+    return Accumulator()
+
+
+# --------------------------------------------------------------------------------
+# Tests
+# --------------------------------------------------------------------------------
+
+@pytest.mark.accumulator
+def test_accumulator_init(accum):
+    assert accum.count == 0
+
+
+@pytest.mark.accumulator
+def test_accumulator_add_one(accum):
+    accum.add()
+    assert accum.count == 1
+
+
+@pytest.mark.accumulator
+def test_accumulator_add_three(accum):
+    accum.add(3)
+    assert accum.count == 3
+
+
+@pytest.mark.accumulator
+def test_accumulator_add_twice(accum):
+    accum.add()
+    accum.add()
+    assert accum.count == 2
+
+
+# @pytest.mark.accumulator
+# def test_accumulator_cannot_set_count_directly(accum):
+#     with pytest.raises(AttributeError, match=r"property 'count' of 'Accumulator' object has no setter") as e:
+#         accum.count = 10
+
+
+
+products = [
+  (2, 3, 6),            # positive integers
+  (1, 99, 99),          # identity
+  (0, 99, 0),           # zero
+  (3, -4, -12),         # positive by negative
+  (-5, -5, 25),         # negative by negative
+  (2.5, 6.7, 16.75)     # floats
+]
+
+@pytest.mark.math
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+  assert a * b == product

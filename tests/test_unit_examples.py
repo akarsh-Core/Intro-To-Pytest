bb4a07f00eacf489c2962cbfee86bfc3eca36e2e
Coredge.io <coredge.io@Coredgeios-MacBook-Pro.local>
2023-08-17 12:43:33 +0530

    added unit test case example

---
+++ tests/test_unit_examples.py
@@ -0,0 +1,23 @@
+import unittest
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_addition(self):
+        result = self.calculator.add(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_subtraction(self):
+        result = self.calculator.subtract(10, 5)
+        self.assertEqual(result, 5)
+
+# if __name__ == "__main__":
+#     unittest.main()
